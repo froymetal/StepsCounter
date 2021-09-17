90ab93ca2cb017b4ca37221418749e327f7e9e42
Froy <froylan.almeida@gmail.com>
2021-09-17 08:23:25 -0500

    Initial Commit

---
+++ StepsCounter/StepsCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepsCounterApp.swift
+//  StepsCounter
+//
+//  Created by Field Employee on 9/17/21.
+//
+
+import SwiftUI
+
+@main
+struct StepsCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
